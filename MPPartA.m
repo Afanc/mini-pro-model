8ec9576e26013add336d287b77457e4bea3ba148
florianwyler <florian.wyler@epfl.ch>
2015-11-15 19:17:12 +0100

    function parameters made accessible

--- MPPartA.m
+++ MPPartA.m
@@ -1,14 +1,17 @@
-%Part A of mini-projet
+%Part A.1 of mini-projet
 
 clear all;
 close all;
 
-Timedelta=[0,1000]; %Time interval in which the script simulates the system
-Initial=[1,2,3]; %Initial conditions of the system
+Timedelta=[0,10000]; %Time interval in which the script simulates the system
+%Initial=[0.1640,0.3312,1.8141]; %Initial conditions of the system
+Initial=[0.2,0.5,2];
 
 options=odeset('RelTol',1e-4,'AbsTol',[1e-4,1e-4,1e-4]);
 
-[T,Y]=ode45(@MPdiffeqA,Timedelta,Initial,options);
+v1i=0.1;
+
+[T,Y]=ode45(MPdiffeqA(v1i),Timedelta,Initial,options);
 
 figure();
 plot(T,Y(:,1),'-',T,Y(:,2),'-',T,Y(:,3),'-');
--- MPdiffeqA.m
+++ MPdiffeqA.m
@@ -1,9 +1,9 @@
 
-function dy= MPdiffeqA(t,y)
+function df= MPdiffeqA(v1i)
 
 %MPdiffeqA contains the differential equation system of part A
 
-v1=0.7;
+v1=v1i;
 v2=0.35;
 v4=0.35;
 v6=0.35;
@@ -14,10 +14,14 @@ K6=1;
 k3=0.7;
 k5=0.7;
 
-dy= zeros(3,1);
-dy(1)= ((v1 * K1^4) / (K1^4 + y(3)^4)) - ((v2 * y(1))/(K2+y(1)));
-dy(2)= (k3*y(1)) - ((v4*y(2))/(K4+y(2)));
-dy(3)= (k5*y(2)) - ((v6*y(3))/(K6+y(3)));
+df=@nested;
+
+    function dy=nested(t,y)
+    dy= zeros(3,1);
+    dy(1)= ((v1 * K1^4) / (K1^4 + y(3)^4)) - ((v2 * y(1))/(K2+y(1)));
+    dy(2)= (k3*y(1)) - ((v4*y(2))/(K4+y(2)));
+    dy(3)= (k5*y(2)) - ((v6*y(3))/(K6+y(3)));
+    end
 
 end
 
